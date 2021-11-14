9a6df4e09ce6e2f4fcbff1cd34b45e2ad0a092cf
AirbladeHD <tamanu0510@gmail.com>
2021-11-14 19:54:58 +0100

    Update config.lua

--- duke_manager/config.lua
+++ duke_manager/config.lua
@@ -1,10 +1,13 @@
 Config = {}
 
 Config.resources = {
+    "mysql-async",
+    "NativeUI",
     "duke_identity",
     "duke_bank",
 }
 
 Config.settings = {
     autoUpdate = false,
+    overwriteDatabase = false,
 }
