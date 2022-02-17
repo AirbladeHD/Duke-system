0446f2b498c79aa74df39da7bf94907c6a530212
AirbladeHD <59345606+AirbladeHD@users.noreply.github.com>
2022-02-17 22:16:16 +0100

    Update duke.sql

--- duke_manager/duke.sql
+++ duke_manager/duke.sql
@@ -11,6 +11,7 @@ CREATE TABLE users (
     coords varchar(100) NOT NULL DEFAULT 0,
     style varchar(255) NOT NULL DEFAULT 0,
     job varchar(50) NOT NULL DEFAULT 0,
+    number varchar(12) NOT NULL
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id)
 );
@@ -22,3 +23,57 @@ CREATE TABLE items (
     id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE apps (
+  id int(11) NOT NULL,
+  icon text NOT NULL,
+  html longtext NOT NULL,
+  js longtext NOT NULL,
+  clientlua longtext NOT NULL,
+  serverlua longtext NOT NULL,
+  rmaccess text NOT NULL,
+  defaultapp int(11) NOT NULL
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE player_apps (
+  pkey int(11) NOT NULL,
+  license varchar(255) NOT NULL,
+  appId int(11) NOT NULL
+  PRIMARY KEY (pkey)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE calllist (
+  id int(11) NOT NULL,
+  fromnumber text NOT NULL,
+  tonumber text NOT NULL,
+  date timestamp NOT NULL DEFAULT current_timestamp(),
+  lic text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE contacts (
+  id int(11) NOT NULL,
+  owner text NOT NULL,
+  name text NOT NULL,
+  nr text NOT NULL
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `vehicles` (
+  `owner` varchar(255) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `displayName` varchar(255) NOT NULL,
+  `manifacturer` varchar(100) NOT NULL,
+  `categorie` varchar(255) NOT NULL,
+  `config` varchar(255) NOT NULL,
+  `upgrades` varchar(255) DEFAULT '0',
+  `currentStatus` varchar(5) NOT NULL DEFAULT 'in',
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `calllist`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id`);
